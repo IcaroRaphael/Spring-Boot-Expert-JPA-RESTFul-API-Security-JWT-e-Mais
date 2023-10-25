a1220adc412d4bd4f489b3f8ec1dffce80f1ebd3
IcaroRaphael <icaro2551@gmail.com>
2023-10-25 16:21:13 -0300

    feat: O Container IOC do Spring Framework

---
+++ vendas/src/main/java/io/github/icaroraphael/vendas/repository/MeuRepository.java
@@ -0,0 +1,7 @@
+package io.github.icaroraphael.vendas.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class MeuRepository {
+}
---
+++ vendas/src/main/java/io/github/icaroraphael/vendas/service/MeuServico.java
@@ -0,0 +1,7 @@
+package io.github.icaroraphael.vendas.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class MeuServico {
+}
