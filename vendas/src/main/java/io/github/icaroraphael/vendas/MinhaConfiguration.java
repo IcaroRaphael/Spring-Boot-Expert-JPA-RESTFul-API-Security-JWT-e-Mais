9dec2fcb9903e00985d304abbccc94872677dd50
IcaroRaphael <icaro2551@gmail.com>
2023-10-30 15:04:06 -0300

    feat: Configuração externalizada - Application Properties

--- vendas/src/main/java/io/github/icaroraphael/vendas/MinhaConfiguration.java
+++ vendas/src/main/java/io/github/icaroraphael/vendas/MinhaConfiguration.java
@@ -6,13 +6,4 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class MinhaConfiguration {
 
-    @Bean(name = "applicationName")
-    public String applicationName(){
-        return "Sistema de Vendas";
-    }
-
-    @Bean(name = "outraConfiguration")
-    public String outraConfiguration(){
-        return "Sistema de Vendas";
-    }
 }
