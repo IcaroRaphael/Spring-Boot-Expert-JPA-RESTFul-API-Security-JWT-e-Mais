70a1552a92c71862e696487483847595f9906264
IcaroRaphael <icaro2551@gmail.com>
2023-11-03 12:37:01 -0300

    feat: Concluindo o cadastro de clientes

--- vendas/src/main/java/io/github/icaroraphael/vendas/domain/repository/ClienteRepository.java
+++ vendas/src/main/java/io/github/icaroraphael/vendas/domain/repository/ClienteRepository.java
@@ -14,6 +14,8 @@ import java.util.List;
 public class ClienteRepository {
     private static String INSERT = "insert into cliente (nome) values (?)";
     private static String SELECT_ALL = "select * from CLIENTE";
+    private static String UPDATE = "update cliente set nome = ? where id = ?";
+    private static String DELETE = "delete from cliente where id = ?";
 
     @Autowired
     private JdbcTemplate jdbcTemplate;
@@ -23,14 +25,35 @@ public class ClienteRepository {
         return cliente;
     }
 
+    public Cliente atualizar(Cliente cliente){
+        jdbcTemplate.update(UPDATE, new Object[]{cliente.getNome(), cliente.getId()});
+        return cliente;
+    }
+
+    public void deletar(Cliente cliente){
+        deletar(cliente.getId());
+    }
+
+    public void deletar(Integer id){
+        jdbcTemplate.update(DELETE, new Object[]{id});
+    }
+
+    public List<Cliente> buscarPorNome(String nome){
+        return jdbcTemplate.query(SELECT_ALL.concat(" where nome like ?"), new Object[]{"%" + nome + "%"}, obterClienteMapper());
+    }
+
     public List<Cliente> obterTodos(){
-        return jdbcTemplate.query(SELECT_ALL, new RowMapper<Cliente>() {
+        return jdbcTemplate.query(SELECT_ALL, obterClienteMapper());
+    }
+
+    private static RowMapper<Cliente> obterClienteMapper() {
+        return new RowMapper<Cliente>() {
             @Override
             public Cliente mapRow(ResultSet rs, int rowNum) throws SQLException {
                 Integer id = rs.getInt("id");
                 String nome = rs.getString("nome");
                 return new Cliente(id, nome);
             }
-        });
+        };
     }
 }
