3f16ea72c269316fa12e623a9c55f59f96a255ff
IcaroRaphael <icaro2551@gmail.com>
2023-11-09 16:52:51 -0300

    feat: Mapeando a entidade Produto

--- vendas/src/main/java/io/github/icaroraphael/vendas/domain/entity/Produto.java
+++ vendas/src/main/java/io/github/icaroraphael/vendas/domain/entity/Produto.java
@@ -1,10 +1,20 @@
 package io.github.icaroraphael.vendas.domain.entity;
 
+
+import jakarta.persistence.*;
+
 import java.math.BigDecimal;
 
+@Entity
+@Table(name = "produto")
 public class Produto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
     private Integer Id;
+    @Column(name = "descricao")
     private String decricao;
+    @Column(name = "preco_unitario")
     private BigDecimal preco;
 
     public Integer getId() {
