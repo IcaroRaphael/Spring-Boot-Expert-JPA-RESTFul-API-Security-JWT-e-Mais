fdbdafd4cfff711ff52350be106b090b424365ba
IcaroRaphael <icaro2551@gmail.com>
2023-11-13 18:54:42 -0300

    feat: Requisição GET com parâmentros e Response Entity

--- vendas/src/main/java/io/github/icaroraphael/vendas/rest/controller/ClienteController.java
+++ vendas/src/main/java/io/github/icaroraphael/vendas/rest/controller/ClienteController.java
@@ -1,19 +1,30 @@
 package io.github.icaroraphael.vendas.rest.controller;
 
 import io.github.icaroraphael.vendas.domain.entity.Cliente;
+import io.github.icaroraphael.vendas.domain.repository.ClienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/api/clientes")
 public class ClienteController {
-    @RequestMapping(
-            value = {"/hello/{nome}", "/api/hello"},
-            method = RequestMethod.POST,
-            consumes = {"application/jason", "application/xml"},
-            produces = {"application/jason", "application/xml"})
+    @Autowired
+    private ClienteRepository repository;
+
+    @GetMapping(value = "/{id}")
     @ResponseBody
-    public String helloCliente(@PathVariable("nome") String nomeCliente, @RequestBody Cliente cliente){
-        return String.format("Hello %s", nomeCliente);
+    public ResponseEntity getClienteById(@PathVariable Integer id){
+        Optional<Cliente> cliente = repository.findById(id);
+        if(cliente.isPresent()){
+            return ResponseEntity.ok( cliente.get() );
+        }
+        return ResponseEntity.notFound().build();
     }
 }
