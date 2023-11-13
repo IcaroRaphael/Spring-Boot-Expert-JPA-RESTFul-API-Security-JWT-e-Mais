c9363a765bb0f1db9070f4e2735b7df02a87c34a
IcaroRaphael <icaro2551@gmail.com>
2023-11-13 18:31:36 -0300

    feat: Mapeando as Requisições com Request Mapping

--- vendas/src/main/java/io/github/icaroraphael/vendas/rest/controller/ClienteController.java
+++ vendas/src/main/java/io/github/icaroraphael/vendas/rest/controller/ClienteController.java
@@ -1,17 +1,19 @@
 package io.github.icaroraphael.vendas.rest.controller;
 
+import io.github.icaroraphael.vendas.domain.entity.Cliente;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/api/clientes")
 public class ClienteController {
-    @RequestMapping(value = "/hello/{nome}", method = RequestMethod.GET)
+    @RequestMapping(
+            value = {"/hello/{nome}", "/api/hello"},
+            method = RequestMethod.POST,
+            consumes = {"application/jason", "application/xml"},
+            produces = {"application/jason", "application/xml"})
     @ResponseBody
-    public String helloCliente(@PathVariable("nome") String nomeCliente){
+    public String helloCliente(@PathVariable("nome") String nomeCliente, @RequestBody Cliente cliente){
         return String.format("Hello %s", nomeCliente);
     }
 }
