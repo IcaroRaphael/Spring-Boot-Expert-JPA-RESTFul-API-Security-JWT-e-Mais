04b9039be2161b72a4f3eadd4ee6562e89d1537d
IcaroRaphael <icaro2551@gmail.com>
2023-11-13 21:39:37 -0300

    feat: Requisicão POST e Request Body

--- vendas/src/main/java/io/github/icaroraphael/vendas/rest/controller/ClienteController.java
+++ vendas/src/main/java/io/github/icaroraphael/vendas/rest/controller/ClienteController.java
@@ -5,10 +5,7 @@ import io.github.icaroraphael.vendas.domain.repository.ClienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
@@ -26,5 +23,12 @@ public class ClienteController {
             return ResponseEntity.ok( cliente.get() );
         }
         return ResponseEntity.notFound().build();
+    }
+
+    @PostMapping
+    @ResponseBody
+    public ResponseEntity save (@RequestBody Cliente cliente){
+        Cliente clienteSalvo = repository.save(cliente);
+        return ResponseEntity.ok(clienteSalvo);
     }
 }
