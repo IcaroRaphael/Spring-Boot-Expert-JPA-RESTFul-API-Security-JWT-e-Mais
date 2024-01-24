4c35343db1027da49706ff3ba90a829b8309c768
IcaroRaphael <icaro2551@gmail.com>
2024-01-24 19:36:37 -0300

    feat: Implementando o método de autenticacao de usuarios

--- vendas/src/main/java/io/github/icaroraphael/vendas/rest/controller/UsuarioController.java
+++ vendas/src/main/java/io/github/icaroraphael/vendas/rest/controller/UsuarioController.java
@@ -1,11 +1,18 @@
 package io.github.icaroraphael.vendas.rest.controller;
 
 import io.github.icaroraphael.vendas.domain.entity.Usuario;
+import io.github.icaroraphael.vendas.exception.SenhaInvalidaException;
+import io.github.icaroraphael.vendas.rest.dto.CredenciaisDTO;
+import io.github.icaroraphael.vendas.rest.dto.TokenDTO;
+import io.github.icaroraphael.vendas.security.jwt.JwtService;
 import io.github.icaroraphael.vendas.service.impl.UsuarioServiceImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import javax.validation.Valid;
 
@@ -15,6 +22,7 @@ import javax.validation.Valid;
 public class UsuarioController {
     private final UsuarioServiceImpl usuarioService;
     private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
@@ -22,5 +30,19 @@ public class UsuarioController {
         String senhaCriptografada = passwordEncoder.encode(usuario.getSenha());
         usuario.setSenha(senhaCriptografada);
         return usuarioService.salvar(usuario);
+    }
+
+    @PostMapping("/auth")
+    public TokenDTO autenticar(@RequestBody CredenciaisDTO credenciais){
+        try{
+            Usuario usuario = Usuario.builder()
+                    .login(credenciais.getLogin())
+                    .senha(credenciais.getSenha()).build();
+            UserDetails usuarioAutenticado = usuarioService.autenticar(usuario);
+            String token = jwtService.gerarToken(usuario);
+            return new TokenDTO(usuario.getLogin(), token);
+        } catch (UsernameNotFoundException | SenhaInvalidaException e ){
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, e.getMessage());
+        }
     }
 }
