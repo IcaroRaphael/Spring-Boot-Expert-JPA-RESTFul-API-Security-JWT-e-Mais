afd7202bfec4346b8a655c56ce15c2dea0ed0952
IcaroRaphael <icaro2551@gmail.com>
2024-02-29 10:45:51 -0300

    feat: Definindo as permissões de acesso via controllers

--- sbootexp-security/src/main/java/io/github/icaroraphael/sbootexpsecurity/config/SecurityConfig.java
+++ sbootexp-security/src/main/java/io/github/icaroraphael/sbootexpsecurity/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package io.github.icaroraphael.sbootexpsecurity.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.core.GrantedAuthorityDefaults;
@@ -17,6 +18,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity(securedEnabled = true)
 public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(
@@ -26,7 +28,6 @@ public class SecurityConfig {
         return http
                 .authorizeHttpRequests(customizer -> {
                     customizer.requestMatchers("/public").permitAll();
-                    customizer.requestMatchers("/admin").hasRole("ADMIN");
                     customizer.anyRequest().authenticated(); //anyRequest tem que ser por ultimo
                 })
                 .httpBasic(Customizer.withDefaults())
