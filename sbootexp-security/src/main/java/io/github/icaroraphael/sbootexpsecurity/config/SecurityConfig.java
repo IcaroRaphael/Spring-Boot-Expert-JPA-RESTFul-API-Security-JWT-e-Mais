287d267daebf319e7ad8c246e39be7471fec8e80
IcaroRaphael <icaro2551@gmail.com>
2024-02-29 17:15:16 -0300

    feat: Integrando a base de usuarios com o Spring Security

--- sbootexp-security/src/main/java/io/github/icaroraphael/sbootexpsecurity/config/SecurityConfig.java
+++ sbootexp-security/src/main/java/io/github/icaroraphael/sbootexpsecurity/config/SecurityConfig.java
@@ -25,6 +25,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(
             HttpSecurity http,
             SenhaMasterAuthenticationProvider senhaMasterAuthenticationProvider,
+            CustomAuthenticationProvider customAuthenticationProvider,
             CustomFilter customFilter) throws Exception{
         return http
                 .csrf(AbstractHttpConfigurer::disable)
@@ -35,6 +36,7 @@ public class SecurityConfig {
                 .httpBasic(Customizer.withDefaults())
                 .formLogin(Customizer.withDefaults())
                 .authenticationProvider(senhaMasterAuthenticationProvider)
+                .authenticationProvider(customAuthenticationProvider)
                 .addFilterBefore(customFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
