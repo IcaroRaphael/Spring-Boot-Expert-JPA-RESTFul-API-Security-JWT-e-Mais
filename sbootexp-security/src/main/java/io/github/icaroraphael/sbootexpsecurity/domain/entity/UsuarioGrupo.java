63d7aae72a1da712f9d13740c7ac62540cf54bdc
IcaroRaphael <icaro2551@gmail.com>
2024-02-29 11:05:24 -0300

    feat: Criando o dominio para o desenvolvimento de um controle de acesso

---
+++ sbootexp-security/src/main/java/io/github/icaroraphael/sbootexpsecurity/domain/entity/UsuarioGrupo.java
@@ -0,0 +1,20 @@
+package io.github.icaroraphael.sbootexpsecurity.domain.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+public class UsuarioGrupo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    @ManyToOne
+    @JoinColumn(name = "id_usuario")
+    private Usuario usuario;
+
+    @ManyToOne
+    @JoinColumn(name = "id_grupo")
+    private Grupo grupo;
+}
