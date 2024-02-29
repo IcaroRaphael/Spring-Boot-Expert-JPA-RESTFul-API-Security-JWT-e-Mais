2bba372023c57cfefe50b48977002e5c81599021
IcaroRaphael <icaro2551@gmail.com>
2024-02-29 16:52:52 -0300

    feat: Criando a logica do AuthenticationProvider para uso no controle de acesso

--- sbootexp-security/src/main/java/io/github/icaroraphael/sbootexpsecurity/domain/repository/UsuarioGrupoRepository.java
+++ sbootexp-security/src/main/java/io/github/icaroraphael/sbootexpsecurity/domain/repository/UsuarioGrupoRepository.java
@@ -1,9 +1,21 @@
 package io.github.icaroraphael.sbootexpsecurity.domain.repository;
 
+import io.github.icaroraphael.sbootexpsecurity.domain.entity.Usuario;
 import io.github.icaroraphael.sbootexpsecurity.domain.entity.UsuarioGrupo;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface UsuarioGrupoRepository extends JpaRepository<UsuarioGrupo, String> {
+    @Query("""
+            select distinct g.nome 
+            from UsuarioGrupo ug 
+            join ug.grupo g 
+            join ug.usuario u
+            where u = ?1
+            """)
+    List<String> findPermissoesByUsuario(Usuario usuario);
 }
--- sbootexp-security/src/main/java/io/github/icaroraphael/sbootexpsecurity/domain/repository/UsuarioRepository.java
+++ sbootexp-security/src/main/java/io/github/icaroraphael/sbootexpsecurity/domain/repository/UsuarioRepository.java
@@ -4,6 +4,9 @@ import io.github.icaroraphael.sbootexpsecurity.domain.entity.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, String> {
+    Optional<Usuario> findByLogin(String login);
 }
